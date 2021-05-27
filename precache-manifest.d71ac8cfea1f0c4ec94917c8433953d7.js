e8ce7014df0d3656821bf07bafac74ae7f8d8d52
Mariya Hubko <alisahbk2@gmail.com>
2021-05-27 23:41:14 +0300

    Updates

---
+++ precache-manifest.d71ac8cfea1f0c4ec94917c8433953d7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc961b41fe014a5b12838f84bd8bcf8a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2279b39555c114beb850",
+    "url": "/static/css/main.bbf9e232.chunk.css"
+  },
+  {
+    "revision": "506d66c88b868351d063",
+    "url": "/static/js/2.4b0441ad.chunk.js"
+  },
+  {
+    "revision": "2279b39555c114beb850",
+    "url": "/static/js/main.03f0ac3c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "540c81bc4ca37ae1efe4b60de51e753f",
+    "url": "/static/media/FuturaPT-Medium.540c81bc.ttf"
+  },
+  {
+    "revision": "5fbd70c5077087b45add01329ea35620",
+    "url": "/static/media/FuturaPT-Medium.5fbd70c5.woff2"
+  },
+  {
+    "revision": "995c64e152e8dc077222766889194951",
+    "url": "/static/media/FuturaPT-Medium.995c64e1.woff"
+  },
+  {
+    "revision": "518c8bb1cf8bf5a60b175e66d66daa95",
+    "url": "/static/media/slide-1.518c8bb1.png"
+  },
+  {
+    "revision": "f92d081f6a216e5dcdde86ea575a1008",
+    "url": "/static/media/slide-2.f92d081f.jpg"
+  },
+  {
+    "revision": "af6966d95b1b6a6272afe17c3c295a98",
+    "url": "/static/media/slide-3.af6966d9.jpg"
+  }
+]);
--- src/сomponents/Footer.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-
-export const Footer = () => <div className='footer'>
-
-</div>;
