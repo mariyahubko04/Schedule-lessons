61244b849d173188847389975b4ce0f5a44d53c0
Mariya Hubko <alisahbk2@gmail.com>
2021-06-05 20:08:46 +0300

    small fixes

--- src/сomponents/LoginPage/ProfilePage/ProfilePageComponents/ExitBtn.js
+++ src/сomponents/LoginPage/ProfilePage/ProfilePageComponents/ExitBtn.js
@@ -1,8 +1,15 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 export const ExitBtn = () => {
+  const history = useHistory();
+
+  const handleExit = () => {
+    history.push('/');
+  };
+
   return (
-    <div className="display-flex exit-btn">
+    <div className="display-flex exit-btn" onClick={handleExit}>
       Вийти з облікового запису
       <img src='images/exit.png' />
     </div>
